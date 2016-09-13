1d05c87f9c5548de461b47b60549bb42b7fc850d
anansharm <anansharm@gmail.com>
2016-09-13 15:29:27 -0700

    a lot of it

--- backEnd/server.js
+++ backEnd/server.js
@@ -1,7 +1,12 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
-var mongo = require('mongodb').MongoClient;
+var mongoose = require('mongoose');
+
+
+var Message = mongoose.model('message', {
+    msg: String
+});
 
 //Middleware
 app.use(bodyParser.json());
@@ -12,17 +17,27 @@ app.use(function(req, res, next){
     next(); //since this is a custom middleware next() is requried so make sure it does not freeze the chain
 });
 
+app.get('/api/message/',  getMesages);
+
 app.post('/api/message/', function(req,res){
     console.log(req.body);
+    var message = new Message(req.body);
+    message.save();
     res.status(200);
 });
 
-mongo.connect("mongodb://localhost:27017/test", function(err, db){
+mongoose.connect("mongodb://localhost:27017/test", function(err, db){
     if(!err){
         console.log('We are connected to the mongo database');
     }
-})
+});
 
 var server = app.listen(5000, function(){
     console.log('Listening on Port', server.address().port);
 });
+
+function getMesages(req, res){
+    Message.find({}).exec(function(err, result){
+        res.send(result);
+    })
+}
